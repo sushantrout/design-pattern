fb581d558eb31868f1a4c89992692f7f5e9a43ac
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-08-13 22:27:56 +0530

    Singleton resource
    Singleton resource

---
+++ signleton-lazy/src/com/tech/ApplicationResource.java
@@ -0,0 +1,22 @@
+package com.tech;
+
+/**
+ * This class is responsible for create and give one singleton object 
+ * At the time of class load the object is create and assign to the INSTACE static variable.
+ * 
+ * @author Sushant
+ *
+ */
+public class ApplicationResource {
+
+	private static final ApplicationResource INSTANCE = new ApplicationResource();
+
+	private ApplicationResource() {
+
+	}
+
+	public ApplicationResource getInstance() {
+		return INSTANCE;
+	}
+
+}
